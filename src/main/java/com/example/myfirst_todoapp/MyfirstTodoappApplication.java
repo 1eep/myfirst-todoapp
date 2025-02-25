a875a2cf79960db4b4f30c4986a921ca886b6610
1eep <ssm.1eep@gmail.com>
2025-02-26 01:24:06 +0900

    DB情報を環境変数に変更

--- src/main/java/com/example/myfirst_todoapp/MyfirstTodoappApplication.java
+++ src/main/java/com/example/myfirst_todoapp/MyfirstTodoappApplication.java
@@ -3,10 +3,17 @@ package com.example.myfirst_todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class MyfirstTodoappApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+
 		SpringApplication.run(MyfirstTodoappApplication.class, args);
 	}
 
